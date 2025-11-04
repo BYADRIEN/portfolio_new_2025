26c0ee7738767920c07c97b0a445cbc3fc5b794e
adrien sellegit config --global user.name adrien <selleadrien@gmail.com>
2025-11-04 16:09:15 +0100

    04/11/2025

--- components/footer/page.tsx
+++ components/footer/page.tsx
@@ -1,9 +1,9 @@
 export default function Footer() {
   return(
-<div class="flex justify-center gap-8">
-  <div class="bg-blue-500 p-4">1</div>
-  <div class="bg-red-500 p-4">2</div>
-  <div class="bg-green-500 p-4">3</div>
+<div className="flex justify-center gap-8">
+  <div className="bg-blue-500 p-4">1</div>
+  <div className="bg-red-500 p-4">2</div>
+  <div className="bg-green-500 p-4">3</div>
 </div>
   );
 }
